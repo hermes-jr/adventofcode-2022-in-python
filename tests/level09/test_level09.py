061f5bad155aacd097fe32ba086d2b5a9dc69a79
Mikhail Antonov <hermes@cyllene.net>
2022-12-09 21:50:02 +0100

    Level 09 part 2

--- tests/level09/test_level09.py
+++ tests/level09/test_level09.py
@@ -1,15 +1,18 @@
 import os
 
 import level09
-
 import utils
 
 td1 = os.path.join(os.path.dirname(__file__), 'in.txt')
-in_data = utils.read_file(td1)
+td2 = os.path.join(os.path.dirname(__file__), 'in2.txt')
+in_data_1 = utils.read_file(td1)
+in_data_2 = utils.read_file(td2)
 
 
 def test_part1():
-    assert level09.p1(in_data) == 13
+    assert level09.p1(in_data_1) == 13
 
-# def test_part2():
-#     assert level09.p2(in_data) == 123
+
+def test_part2():
+    assert level09.p2(in_data_1) == 1
+    assert level09.p2(in_data_2) == 36
